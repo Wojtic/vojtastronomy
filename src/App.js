bf5771d6853352dddb5f4ee9ce957e0355ad2584
Wojtic <vojtacerny012@gmail.com>
2022-08-28 13:25:25 +0200

    Moved filenames into separate json

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Page from "./components/Page";
-
-const filenames = [
-  { path: "Home", filename: "Home (web)" },
-  { path: "Mlok", filename: "Mlok (web)" },
-];
+import filenames from "./files/names.json";
 
 function App() {
   const [ENGLISH, setENGLISH] = useState(false);
