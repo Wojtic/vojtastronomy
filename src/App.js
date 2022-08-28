6e814926cf5c94e728392abe3f6ddc0a8fdbc2b9
Wojtic <vojtacerny012@gmail.com>
2022-08-28 21:29:50 +0200

    html for navigation

--- src/App.js
+++ src/App.js
@@ -8,36 +8,54 @@ function App() {
   const [ENGLISH, setENGLISH] = useState(false);
 
   return (
-    <>
-      <input type="checkbox" onClick={() => setENGLISH(!ENGLISH)} />
-      English?
-      <Router>
-        <Routes>
-          {filenames.map((page) => {
-            return (
-              <Route
-                path={"/" + page.path}
-                element={<Page name={page.filename} english={ENGLISH} />}
-              />
-            );
-          })}
-          <Route
-            path="/"
-            element={<Page name={"Home (web)"} english={ENGLISH} />}
-          ></Route>
-          <Route
-            path="*"
-            element={
-              <>
-                <br />
-                <Link to="/Home">Home</Link>
-                <p>404</p>
-              </>
-            }
-          ></Route>
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/Home">{ENGLISH ? "Home" : "Domov"}</Link>
+          </li>
+          <li>
+            <Link to="/Mlok">
+              {ENGLISH ? "Mlok (telescope)" : "Mlok (teleskop)"}
+            </Link>
+          </li>
+          <li>
+            <Link to="/About">{ENGLISH ? "About" : "O stránce"}</Link>
+          </li>
+          <li>
+            <Link to="/About_me">{ENGLISH ? "About me" : "O mně"}</Link>
+          </li>
+          <li>
+            <input type="checkbox" onClick={() => setENGLISH(!ENGLISH)} />
+            English?
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        {filenames.map((page) => {
+          return (
+            <Route
+              path={"/" + page.path}
+              element={<Page name={page.filename} english={ENGLISH} />}
+            />
+          );
+        })}
+        <Route
+          path="/"
+          element={<Page name={"Home (web)"} english={ENGLISH} />}
+        ></Route>
+        <Route
+          path="*"
+          element={
+            <>
+              <br />
+              <Link to="/Home">Home</Link>
+              <p>404</p>
+            </>
+          }
+        ></Route>
+      </Routes>
+    </Router>
   );
 }
 
