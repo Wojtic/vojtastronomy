1167d15c7bbd4c799aae6a4556950806ab1403a0
Wojtic <vojtacerny012@gmail.com>
2022-08-28 14:59:44 +0200

    Github pages and python creates names.json

--- src/files/get_files.py
+++ src/files/get_files.py
@@ -2,8 +2,17 @@ from os import listdir
 import shutil
 
 PATH = "/home/vojta/Dokumenty/škola/Poznamky/Poznámky/"
-OUTPUT_PATH = "/home/vojta/Dokumenty/programovani/web/vojtastronomy/markdown/"
+OUTPUT_PATH = "/home/vojta/Dokumenty/programovani/web/vojtastronomy/src/files/markdown/"
+
+files = []
 
 for f in listdir(PATH):
-  if '(web)' in f:
-    shutil.copyfile(PATH + f, OUTPUT_PATH + f) 
+    if '(web)' in f:
+        shutil.copyfile(PATH + f, OUTPUT_PATH + f)
+        files.append('{"path":"' + f.replace(" ", "_")
+                     [:len(f) - 9] + '", "filename": "' + f[:len(f) - 3] + '"}')
+
+
+f = open("/home/vojta/Dokumenty/programovani/web/vojtastronomy/src/files/names.json", "w")
+f.write("[" + ",".join(files) + "]")
+f.close()
